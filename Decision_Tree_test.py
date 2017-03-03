1bfd2c3c8505f36a1b774ea94aac73d8f3cf9cb8
evillog <evillog@qq.com>
2017-03-03 22:41:40 +0800

    决策树---Sex，Age（>9.5）, SibSp(>2.5) Score：0.77990

--- Decision_Tree_test.py
+++ Decision_Tree_test.py
@@ -21,9 +21,13 @@ def create_tree():
     decision_tree = TreeNode('Sex')
     sex_left = TreeNode('Age')
     sex_right = TreeNode('Survived')
-    male_left = TreeNode('Survived')
-    male_right = TreeNode('Died')
+    male_left = TreeNode('Died')
+    male_right = TreeNode('SibSp')
+    sibsp_left = TreeNode('Died')
+    sibsp_right = TreeNode('Survived')
 
+    male_right.left = sibsp_left
+    male_right.right = sibsp_right
     sex_left.left = male_left
     sex_left.right = male_right
     decision_tree.left = sex_left
@@ -39,9 +43,17 @@ def is_male(sex):
         return 0
 
 
-# 判断年龄是否小于等于一岁
+# 判断年龄是否大于等于9.5岁
 def is_baby(age):
-    if age <= 9.5:
+    if age >= 9.5:
+        return 1
+    else:
+        return 0
+
+
+# 判断Sibsp是否大于等于3
+def is_sibsp(sibsp):
+    if sibsp >= 3:
         return 1
     else:
         return 0
@@ -52,12 +64,12 @@ def is_baby(age):
 #     is_survived(1, 0.5, decision_tree)
 
 
-def is_survived(sex, age, decision_tree):
-    r = DFS_tree(decision_tree, 0, sex, age)
+def is_survived(sex, age, sibsp, decision_tree):
+    r = DFS_tree(decision_tree, 0, sex, age, sibsp)
     return r
 
 
-def DFS_tree(node, deep, sex, age):
+def DFS_tree(node, deep, sex, age, sibsp):
     r = 0
     if node.val == 'Survived':
         r = 1
@@ -67,15 +79,22 @@ def DFS_tree(node, deep, sex, age):
     elif node.val == 'Sex':
         result = is_male(sex)
         if result:
-            r =DFS_tree(node.left, (deep + 1), sex, age)
+            r =DFS_tree(node.left, (deep + 1), sex, age, sibsp)
         else:
-            r =DFS_tree(node.right, (deep + 1), sex, age)
+            r =DFS_tree(node.right, (deep + 1), sex, age, sibsp)
     elif node.val == 'Age':
         result = is_baby(age)
         if result:
-            r = DFS_tree(node.left, (deep + 1), sex, age)
+            r = DFS_tree(node.left, (deep + 1), sex, age, sibsp)
         else:
-            r = DFS_tree(node.right, (deep + 1), sex, age)
+            r = DFS_tree(node.right, (deep + 1), sex, age, sibsp)
+    elif node.val == 'SibSp':
+        result = is_sibsp(sibsp)
+        if result:
+            r = DFS_tree(node.left, (deep + 1), sex, age, sibsp)
+        else:
+            r = DFS_tree(node.right, (deep + 1), sex, age, sibsp)
+
     return r
 
 
@@ -94,7 +113,7 @@ if __name__ == "__main__":
                 age = 200
             else:
                 age = float(row['Age'])
-            result = is_survived(sex, age, decision_tree)
+            result = is_survived(sex, age, int(row['SibSp']), decision_tree)
             new_dict['PassengerId'] = row['PassengerId']
             new_dict['Survived'] = result
 
